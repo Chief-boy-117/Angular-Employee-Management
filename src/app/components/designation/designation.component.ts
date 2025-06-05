1e242e2e8a79f3dff7a9c374f67a865832de9c52
Chief-boy-117 <avihrikb@gmail.com>
2025-06-05 21:41:04 +0530

    Reapply "API calls for Clients"
    This reverts commit a7e8fe74ff61edca133bcee1814d2dfe4655979c.

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { APIResponseModel } from '../../model/interface/role';
+import { IDesignation } from '../../model/interface/designation';
 
 @Component({
   selector: 'app-designation',
@@ -6,6 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css'
 })
-export class DesignationComponent {
+export class DesignationComponent implements OnInit {
+  masterService = inject(MasterService);
+  designationList: IDesignation[] = [];
+  isLoader: boolean = true;
 
+  ngOnInit(): void {
+    this.masterService.getDesignations().subscribe((res: APIResponseModel) => {
+      this.designationList = res.data;
+      this.isLoader = false;
+    }, error => {
+      alert("API error");
+      this.isLoader = false;
+    })
+  }
 }
